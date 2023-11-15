b8a77df54d37c2bc881374a03f16fa3c2d67fa98
Psychopatz <bpatrickoliverb@gmail.com>
2023-11-15 23:10:45 +0800

    Add springboot backend intialization

---
+++ ExtracurricularExplorer/src/main/java/com/csit321/ExtracurricularExplorer/ExtracurricularExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.csit321.ExtracurricularExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtracurricularExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtracurricularExplorerApplication.class, args);
+	}
+
+}
