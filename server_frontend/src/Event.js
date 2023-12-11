34885ad708e2c9556bd3f6a561f8c608afb667b0
Psychopatz <bpatrickoliverb@gmail.com>
2023-12-11 23:57:41 +0800

    update Event.js

--- server_frontend/src/Event.js
+++ server_frontend/src/Event.js
@@ -1,6 +1,7 @@
 import BoxContainer from "./component/BoxContainer";
 
 const Event = () => {
+    document.title =  'Browse CIT Events';
     return ( 
         <BoxContainer>
         <h1 className="pageName">Events</h1>
