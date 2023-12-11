a2f57350ae066fa67bc006805546ad825c784a0d
Psychopatz <bpatrickoliverb@gmail.com>
2023-12-12 07:28:15 +0800

    implement Account.js (Unfinished)

--- server_frontend/src/Account.js
+++ server_frontend/src/Account.js
@@ -1,48 +1,71 @@
+import React, { useEffect, useState } from 'react';
 import ApiFetch from "./hooks/ApiFetch";
 import SetOption from "./util/SetOption";
 import useLocalStorage from "./hooks/useLocalStorage";
 import UploadBtn from "./component/UploadBtn";
-import { useState } from "react";
+import { Box, Typography } from "@mui/material";
+import PhotoDisplayer from './component/PhotosDisplayer';
 
 const Account = () => {
     const [storedValue, setStoredValue, clearStoredValue] = useLocalStorage('loginSession', '1');
-    const currentUrl = SetOption("url")+"/account/user/"+storedValue
+    const currentUrl = SetOption("url")+"/account/user/"+ storedValue
     const [uploadError, setUploadError] = useState(null);
     const [responseData, setResponseData] = useState('Empty');
+    const [apiData, setApiData] = useState(null);
+
+    const [loading, setLoading] = useState(true);
+    const [userProfile, setUserProfile] = useState(
+        {
+            img: 'http://127.0.0.1:8080/image/fileSystem/',
+            title: 'User Profile'
+        })
 
     const handleUploadError = (error) => {
-    setUploadError(error);
-  };
+      setUploadError(error);
+    };
 
     const handleResponseData = (data) => {
-    setResponseData(data);
-  };
+      setResponseData(data);
+    };
+
+    const handleApiData = (data) => {
+      setApiData(data);
+      setLoading(false);
+    };
+
+    useEffect(() => {
+        setUserProfile(
+            {
+            img: 'http://127.0.0.1:8080/image/fileSystem/' + apiData.profilePicture,
+            title: apiData.fName+" Profile"
+            })
+      console.log(apiData);
+    }, [apiData]);
 
 
-    
     return (  
         <>
-        <ApiFetch url={currentUrl} method="GET">
+          <ApiFetch url={currentUrl} method="GET" onDataFetched={handleApiData}>
             {({ responseData, loading }) => (
-                <div>
-                    {/* Your component JSX */}
-                    {loading ? (
-                        <p>Loading...</p>
-                    ) : (
-                        <pre>{JSON.stringify(responseData, null, 2)}</pre>
-                    )}
-                </div>
+              <div>
+                {/* Optional: Render loading state or other UI while data is being fetched */}
+              </div>
             )}
-        </ApiFetch>
-        <UploadBtn
-        onUploadError={handleUploadError}
-        onResponseData={handleResponseData}
-      />
-      <p>Upload Error: {uploadError}</p>
-      <p>Response Data: {responseData}</p>
-            
-            </>
+          </ApiFetch>
+          <Box>
+            {loading && <PhotoDisplayer photos={{userProfile}} />}
+          </Box>
+
+          <UploadBtn
+            onUploadError={handleUploadError}
+            onResponseData={handleResponseData}
+          />
+          <p>Upload Error: {uploadError}</p>
+          <p>Response Data: {responseData}</p>
+          
+        </>
+        
     );
 }
- 
+
 export default Account;
