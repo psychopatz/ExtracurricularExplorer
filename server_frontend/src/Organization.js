0792f99b06a51dc4b68a5972d6df9d1fb44d0f4e
Psychopatz <bpatrickoliverb@gmail.com>
2023-12-11 23:58:55 +0800

    update Organization.js

--- server_frontend/src/Organization.js
+++ server_frontend/src/Organization.js
@@ -1,6 +1,7 @@
 import BoxContainer from "./component/BoxContainer";
 
 const Organization = () => {
+    document.title =  'Browse CIT Organization';
     return (  
         <BoxContainer>
         <h1 className="pageName">Organization</h1>
