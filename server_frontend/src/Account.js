d8a21d3ab4450cf59c204ebd721a37e76449887b
Psychopatz <bpatrickoliverb@gmail.com>
2023-12-12 13:35:18 +0800

    improve Account.js and added placeholder items

--- server_frontend/src/Account.js
+++ server_frontend/src/Account.js
@@ -1,67 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import ApiFetch from "./hooks/ApiFetch";
-import SetOption from "./util/SetOption";
 import useLocalStorage from "./hooks/useLocalStorage";
-import UploadBtn from "./component/UploadBtn";
 import { Box, Typography } from "@mui/material";
 import PhotoDisplayer from './component/PhotosDisplayer';
+import useApiFetch from './hooks/useApiFetch';
+
 
 const Account = () => {
-    const [storedValue, setStoredValue, clearStoredValue] = useLocalStorage('loginSession', '1');
-    const currentUrl = SetOption("url")+"/account/user/"+ storedValue
-    const [uploadError, setUploadError] = useState(null);
-    const [responseData, setResponseData] = useState('Empty');
-    const [apiData, setApiData] = useState(null);
-
-    const [loading, setLoading] = useState(true);
-    const [userProfile, setUserProfile] = useState(
-        {
-            img: 'http://127.0.0.1:8080/image/fileSystem/',
-            title: 'User Profile'
-        })
-
-    const handleUploadError = (error) => {
-      setUploadError(error);
-    };
-
-    const handleResponseData = (data) => {
-      setResponseData(data);
-    };
-
-    const handleApiData = (data) => {
-      setApiData(data);
-      setLoading(false);
-    };
-
+    const [storedValue, setStoredValue, clearStoredValue] = useLocalStorage('loginSession', '-2');
+    //User Data
+    const { data, loading, error, fetchData } = useApiFetch();
     useEffect(() => {
-        setUserProfile(
-            {
-            img: 'http://127.0.0.1:8080/image/fileSystem/' + apiData.profilePicture,
-            title: apiData.fName+" Profile"
-            })
-      console.log(apiData);
-    }, [apiData]);
+        fetchData("/account/user/"+storedValue);
+        
+        console.log(data)
+        console.log(loading)
+        console.log(error)
+    }, []);
 
+    //Upload Photo
 
+//Object { id: 50, 
+//firstName: "Charles", 
+//lastName: "Hordista", 
+//email: "Chad@cit.edu", 
+//password: "12345678", 
+//profilePicture: null }
     return (  
         <>
-          <ApiFetch url={currentUrl} method="GET" onDataFetched={handleApiData}>
-            {({ responseData, loading }) => (
-              <div>
-                {/* Optional: Render loading state or other UI while data is being fetched */}
-              </div>
-            )}
-          </ApiFetch>
+          
           <Box>
-            {loading && <PhotoDisplayer photos={{userProfile}} />}
+                {data && (
+                <>
+                <PhotoDisplayer photos={
+                  {
+                      img: 'http://127.0.0.1:8080/image/fileSystem/' + data.profilePicture,
+                      title: data.firstName+" Profile"
+                  }} />
+                <Typography>Moderator Name: {data.firstName} {data.lastName}</Typography>
+                <Typography>Email: {data.email}</Typography>
+                
+                </>)
+                  
+                }
+          
           </Box>
+          
 
-          <UploadBtn
-            onUploadError={handleUploadError}
-            onResponseData={handleResponseData}
-          />
-          <p>Upload Error: {uploadError}</p>
-          <p>Response Data: {responseData}</p>
+        
+          
           
         </>
         
