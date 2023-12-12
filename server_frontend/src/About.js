b1bced4060028c873c521d905604f16be29ebfd0
Psychopatz <bpatrickoliverb@gmail.com>
2023-12-12 13:35:35 +0800

    improve About.js

--- server_frontend/src/About.js
+++ server_frontend/src/About.js
@@ -1,12 +1,22 @@
 import BoxContainer from "./component/BoxContainer";
-import { Box, Button } from "@mui/material";
-import useGet from "./hooks/useGet";
-import SetOption from "./util/SetOption";
+import { Box, Button, Typography } from "@mui/material";
+import useApiFetch from "./hooks/useApiFetch";
+import { useEffect } from "react";
+import PhotoDisplayer from "./component/PhotosDisplayer";
 
 const About = () => {
     document.title =  'About';
+    
+    const { data, loading, error, fetchData } = useApiFetch();
+    useEffect(() => {
+        fetchData('/data/get/about');
+    }, []);
 
-    const {content, isPending, error} = useGet("/data/get/about");
+    console.log(data)
+    console.log(loading)
+    console.log(error)
+
+
     
     return ( 
         <>
@@ -14,8 +24,8 @@ const About = () => {
             <h1 className="pageName">About</h1>
         </BoxContainer>
         <Box>
-            <p>{content.type}</p>
-            <p>{content.content}</p>
+            
+            
 
         </Box>
         
